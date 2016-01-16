1c215336ed23d066f945923c15da500b453e610a
Joshua David Jones <the8bitbass@gmail.com>
2016-01-17 00:19:24 +1100

    Add the IEnemy interface

---
+++ EMP/Enemy/IEnemy.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace EMP
+{
+    interface IEnemy
+    {
+        Texture2D Texture { get; set; }
+        Vector2 Position { get; set; }
+
+        void Update();
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
