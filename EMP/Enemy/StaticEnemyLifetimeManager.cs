26c50989b0a242bb3711b53c2df94ebcba09f970
Joshua David Jones <the8bitbass@gmail.com>
2016-01-16 22:35:43 +1100

    Add enemy active and inactive tracking lists

--- EMP/Enemy/StaticEnemyLifetimeManager.cs
+++ EMP/Enemy/StaticEnemyLifetimeManager.cs
@@ -1,6 +1,12 @@
 ﻿namespace EMP
+﻿using System.Collections.Generic;
+
+namespace EMP
 {
     class StaticEnemyLifetimeManager
     {
+        private List<BoringEnemy> activeEnemies;
+        private List<BoringEnemy> inactiveEnemies;
+
     }
 }
